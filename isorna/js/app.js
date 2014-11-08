88117e48c3a0c70c71266a7475a8afa2e157a13c
Alvaro Isorna <alvaro.isorna@gmail.com>
2014-11-08 12:02:53 +0000

    Week 2

--- isorna/js/app.js
+++ isorna/js/app.js
@@ -2,7 +2,7 @@
 'use scrict';
 
 // Use Fluent code style guide, no globals!
-angular.module('myAppModule', [/*'ngRoute'*/])
+angular.module('myAppModule', ['ngRoute'])
     .controller('MenuController', ['$scope', function ($scope) {
         // $scope variables
         $scope.URL_MENU = 'templates/menu.html';
@@ -13,23 +13,30 @@ angular.module('myAppModule', [/*'ngRoute'*/])
             $scope.menuItem = '';
         };
     }])
-    /*
-    .config(['$routeProvider', function ($routeProvider) {
+    .controller('HomePageController', ['$scope', function ($scope) {
+        console.log('home cargada');
+    }])
+    .controller('OtherPageController', ['$scope', function ($scope) {
+        console.log('otra pagina cargada');
+    }])
+    .config(['$routeProvider', function ($routeProvider, $locationProvider) {
         $routeProvider 
         // Home
-        .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+        .when("/", {templateUrl: "partials/home.html", controller: "HomePageController"});
         // Pages
-        .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-        .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
+        //.when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+        //.when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
         // Blog
-        .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
-        .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+        //.when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
+        //.when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
         // else 404
-        .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"}); 
-    }]);
+        //.otherwise("/404", {templateUrl: "partials/404.html", controller: "OtherPageController"});
+        // configure html5 to get links working on jsfiddle
+        $locationProvider.html5Mode(true);
+    }])
     .controller('BlogCtrl', function ($scope, $location, $http) {
         console.log("Blog Controller reporting for duty.");
     })
     .controller('PageCtrl', function ($scope, $location, $http) {
         console.log("Page Controller reporting for duty.");
-    });*/;
+    });
