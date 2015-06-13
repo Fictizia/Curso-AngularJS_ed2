e8a647a7c0402495069d64907dbb605523ecc760
Aitor García <aitor_garcia_69@hotmail.com>
2015-06-13 12:32:41 +0000

    cuarta semana

--- aitoromg/webapp/db/index.js
+++ aitoromg/webapp/db/index.js
@@ -14,10 +14,48 @@ angular.module('modulo.db', ['ngRoute', "firebase"])
             }
             ]
     })
-    .controller('DBCont', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+    .factory('DBService', ['$firebaseArray', function($firebaseArray) {
         var fb_db = new Firebase("https://aitoromg-angularjs.firebaseio.com/users");
-        // download the data into a local object
-        $scope.users = $firebaseArray(fb_db);
+        
+        return {
+            getChat: function() {
+                var fb_Array = $firebaseArray(fb_db)
+                
+                
+                fb_Array.$loaded().then(function(poDB) {
+                    console.log('DB cargada', poDB);
+                    poDB === fb_Array; // true
+                })
+                .catch(function(error) {
+                    console.log("Error:", error);
+                });
+                
+                return fb_Array;
+            },
+            ordenar: function() {
+                var fb_Array = $firebaseArray(fb_db.orderByChild('name'));
+                
+                return fb_Array;
+            }
+        }
+    }])
+    .controller('ChatCont', ['$scope', 'DBService', function($scope, DBService) {
+        $scope.users = DBService.getChat();
+        $scope.borrarUser = function (user) {
+            $scope.users.$remove(user).then(function(ref) {
+                console.log('Se ha borrado el usuario ', user.$id);
+                ref.key() === user.$id; // true
+            });
+        };
+        $scope.actualizarUser = function (user) {
+            $scope.users.$save(user).then(function(ref) {
+                console.log('Se ha actualizado el usuario ', user.$id);
+                ref.key() === user.$id; // true
+            });
+        };
+        $scope.ordenarUser = function (user){
+            $scope.users = DBService.ordenar();
+        }
     }])
     .controller('UserCont', ['$scope', function($scope) {
         $scope.user = {};
